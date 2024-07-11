90a9ebc785ddfd018b191a43cbede18cdf037a73
CoolPenwin <coolPenwin@proton.me>
2024-07-11 20:22:17 +0200

    porque error?

--- src/app.js
+++ src/app.js
@@ -27,37 +27,36 @@ window.onload = () => {
   
   
 function DomainGenerator () {
-  let pre = ["A", "The", "Some", "My", "Your", "Our", "Their"];
-  let adj = ["squeeze", "awesome", "funny", "Cool", "crazy", "silly", "wacky", "bizarre",];
-    let suj = ["penwin", "octopuss", "potato", "tomato", "cow", "cat","unicorn","dinosaur","ninja","robot", ];
-    let dom = [".com", ".es",".lol",".fun",".haha",".wtf",];
+    let prep = ["The", "A", "Some", "My", "Your", "Our", "Their"];
+    let adj = ["Cool", "squeeze", "awesome", "funny", "crazy", "silly", "wacky", "bizarre"];
+    let suj = ["penwin", "octopuss", "potato", "tomato", "cow", "cat","unicorn","dinosaur","ninja","robot"];
+    let dom = [".lol", ".com", ".es",".lol",".fun",".haha",".wtf"];
 
-    let preIndex ;
-    let adjIndex ;
-    let sujIndex ;
-    let domIndex ; 
+    let preIndex = 0;
+    let adjIndex = 0;
+    let sujIndex = 0;
+    let domIndex = 0; 
     
-    if (todos=true) {
-         //
-         preIndex = ;
-         adjIndex = ;
-           sujIndex = ;
-           domIndex = ;       
+    // if (todos=true) {
+    //      //
+    //     //  preIndex = ;
+    //     //  adjIndex = ;
+    //     //    sujIndex = ;
+    //     //    domIndex = ;       
            
         
-      } else {
-               // genera de forma aleatoria 
-           preIndex = Math.floor(Math.random() * pre.length);
-           adjIndex = Math.floor(Math.random() * adj.length);
-           sujIndex = Math.floor(Math.random() * suj.length);
-           domIndex = Math.floor(Math.random() * dom.length);  
+    //   } else {
+    //            // genera de forma aleatoria 
+    //        preIndex = Math.floor(Math.random() * pre.length);
+    //        adjIndex = Math.floor(Math.random() * adj.length);
+    //        sujIndex = Math.floor(Math.random() * suj.length);
+    //        domIndex = Math.floor(Math.random() * dom.length);  
           
-        }
+    //     }
         
    
     
-  return {"tu nuevo dominio es:  "+
-    pre[preIndex] + adj[adjIndex] + suj[sujIndex] + dom[domIndex]
+  return {"tu nuevo dominio es:  "prep[preIndex] + adj[adjIndex] + suj[sujIndex] + dom[domIndex]
   }  
   
 };
